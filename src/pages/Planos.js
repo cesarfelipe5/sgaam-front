63a1e19632874ba541684217f3a4ba6ff96ddc87
Cesar Felipe <53799947+cesarfelipe5@users.noreply.github.com>
2024-11-22 11:21:15 -0300

    Novas alteracoes (#2)
    * Co-authored-by: Cesar Felipe <cesarfelipe5@users.noreply.github.com>
    
    * correcao
    
    * correcao status
    
    * tabela_planos
    
    ---------
    
    Co-authored-by: Fabian Filipe <fabfilipelino@gmail.com>

--- src/pages/Planos.js
+++ src/pages/Planos.js
@@ -1,61 +1,65 @@
-import { Button, DatePicker, Form, Input, Modal, Select, Table } from "antd";
-import moment from "moment";
 import React, { useState } from "react";
+import { Button, Card, Form, Input, Modal, Select, Table } from "antd";
 import DrawerMenu from "../components/DrawerMenu";
 
 const { Option } = Select;
-const { Search } = Input;
 
 const Planos = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [editingRecord, setEditingRecord] = useState(null);
-  const [searchText, setSearchText] = useState(""); // Estado para o campo de busca
-  const [plans, setPlans] = useState([]);
+  const [plans, setPlans] = useState([
+    {
+      key: "1",
+      nome: "Plano Básico",
+      modalidades: ["Karatê", "Judô"],
+      valor: "R$ 100,00",
+    },
+    {
+      key: "2",
+      nome: "Plano Avançado",
+      modalidades: ["Jiu-Jitsu", "Boxe"],
+      valor: "R$ 200,00",
+    },
+  ]);
+  const [deleteRecord, setDeleteRecord] = useState(null); // Estado para o modal de confirmação de exclusão
 
   const [form] = Form.useForm();
 
   const handleAddPlan = () => {
     setIsModalVisible(true);
-
     setEditingRecord(null);
-
     form.resetFields();
   };
 
   const handleEditPlan = (record) => {
     setIsModalVisible(true);
-
     setEditingRecord(record);
-
-    form.setFieldsValue({
-      ...record,
-      dataPagamento: moment(record.dataPagamento, "DD/MM/YYYY"),
-    });
+    form.setFieldsValue(record);
   };
 
   const handleDeletePlan = (key) => {
     setPlans(plans.filter((item) => item.key !== key));
+    setDeleteRecord(null);
+  };
+
+  const confirmDelete = (record) => {
+    setDeleteRecord(record);
   };
 
   const handleSave = () => {
     form.validateFields().then((values) => {
       const newData = [...plans];
       if (editingRecord) {
-        // Editando um pagamento existente
+        // Editando um plano existente
         const index = newData.findIndex(
           (item) => item.key === editingRecord.key
         );
-        newData[index] = {
-          ...editingRecord,
-          ...values,
-          dataPagamento: values.dataPagamento.format("DD/MM/YYYY"),
-        };
+        newData[index] = { ...editingRecord, ...values };
       } else {
-        // Adicionando um novo pagamento
+        // Adicionando um novo plano
         newData.push({
           key: `${newData.length + 1}`,
           ...values,
-          dataPagamento: values.dataPagamento.format("DD/MM/YYYY"),
         });
       }
       setPlans(newData);
@@ -63,54 +67,50 @@ const Planos = () => {
     });
   };
 
-  // Filtragem dos dados com base na pesquisa
-  const filteredData = plans.filter((aluno) =>
-    aluno.nome.toLowerCase().includes(searchText.toLowerCase())
-  );
-
   const columns = [
     {
-      title: "Nome do Aluno",
+      title: "Nome do Plano",
       dataIndex: "nome",
       key: "nome",
     },
     {
-      title: "Data do Pagamento",
-      dataIndex: "dataPagamento",
-      key: "dataPagamento",
+      title: "Modalidades",
+      dataIndex: "modalidades",
+      key: "modalidades",
+      render: (modalidades) =>
+        modalidades.map((mod) => (
+          <Card
+            key={mod}
+            style={{
+              display: "inline-block",
+              marginRight: 8,
+              border: "1px solid #ccc",
+              borderRadius: 4,
+              padding: "4px 8px",
+            }}
+          >
+            {mod}
+          </Card>
+        )),
     },
     {
-      title: "Valor Pago",
-      dataIndex: "valorPago",
-      key: "valorPago",
-    },
-    {
-      title: "Valor do Plano",
-      dataIndex: "valorPlano",
-      key: "valorPlano",
-    },
-    {
-      title: "Recebedor",
-      dataIndex: "recebedor",
-      key: "recebedor",
+      title: "Valor",
+      dataIndex: "valor",
+      key: "valor",
     },
     {
       title: "Ações",
       key: "acoes",
-      render: (text, record) => (
+      render: (_, record) => (
         <>
           <Button
-            style={{ marginRight: 10 }}
             type="link"
             onClick={() => handleEditPlan(record)}
+            style={{ marginRight: 8 }}
           >
             Editar
           </Button>
-          <Button
-            type="link"
-            danger
-            onClick={() => handleDeletePlan(record.key)}
-          >
+          <Button type="link" danger onClick={() => confirmDelete(record)}>
             Excluir
           </Button>
         </>
@@ -121,32 +121,24 @@ const Planos = () => {
   return (
     <>
       <DrawerMenu />
-
-      <div style={{ padding: "20px" }}>
+      <div style={{ padding: 20 }}>
         <Button
           type="primary"
-          style={{ backgroundColor: "black", marginBottom: "10px" }}
+          icon="+"
+          style={{
+            marginBottom: 20,
+            backgroundColor: "black",
+            color: "white",
+            border: "none",
+          }}
           onClick={handleAddPlan}
         >
-          Novo Pagamento
+          Novo Plano
         </Button>
-
-        {/* Campo de pesquisa */}
-        <Search
-          placeholder="Buscar por nome do aluno"
-          onChange={(e) => setSearchText(e.target.value)}
-          style={{
-            width: 300,
-            marginBottom: "1px",
-            marginTop: "10px",
-            display: "block",
-          }}
-        />
-
-        <Table columns={columns} dataSource={filteredData} />
+        <Table columns={columns} dataSource={plans} />
 
         <Modal
-          title={editingRecord ? "Editar plano" : "Novo plano"}
+          title={editingRecord ? "Editar Plano" : "Novo Plano"}
           open={isModalVisible}
           onCancel={() => setIsModalVisible(false)}
           onOk={handleSave}
@@ -155,48 +147,50 @@ const Planos = () => {
         >
           <Form form={form} layout="vertical">
             <Form.Item
-              label="Nome do Aluno"
+              label="Nome do Plano"
               name="nome"
+              rules={[{ required: true, message: "Por favor, insira o nome" }]}
+            >
+              <Input placeholder="Nome do plano" />
+            </Form.Item>
+            <Form.Item
+              label="Modalidades"
+              name="modalidades"
               rules={[
-                { required: true, message: "Por favor, selecione o aluno" },
+                { required: true, message: "Por favor, selecione as modalidades" },
               ]}
             >
               <Select
-                showSearch
-                placeholder="Selecione um aluno"
-                optionFilterProp="children"
-                filterOption={(input, option) =>
-                  option.children.toLowerCase().indexOf(input.toLowerCase()) >=
-                  0
-                }
+                mode="multiple"
+                placeholder="Selecione as modalidades"
+                style={{ width: "100%" }}
               >
-                <Option value="João Silva">João Silva</Option>
-                <Option value="Maria Souza">Maria Souza</Option>
-                <Option value="Carlos Pereira">Carlos Pereira</Option>
+                <Option value="Karatê">Karatê</Option>
+                <Option value="Judô">Judô</Option>
+                <Option value="Jiu-Jitsu">Jiu-Jitsu</Option>
+                <Option value="Boxe">Boxe</Option>
               </Select>
             </Form.Item>
             <Form.Item
-              label="Valor Pago"
-              name="valorPago"
-              rules={[
-                { required: true, message: "Por favor, insira o valor pago" },
-              ]}
+              label="Valor"
+              name="valor"
+              rules={[{ required: true, message: "Por favor, insira o valor" }]}
             >
-              <Input prefix="R$" placeholder="Valor em BRL" />
-            </Form.Item>
-            <Form.Item
-              label="Data do Pagamento"
-              name="dataPagamento"
-              rules={[
-                {
-                  required: true,
-                  message: "Por favor, insira a data do pagamento",
-                },
-              ]}
-            >
-              <DatePicker format="DD/MM/YYYY" />
+              <Input prefix="R$" placeholder="Valor do plano" />
             </Form.Item>
           </Form>
+        </Modal>
+
+        <Modal
+          title="Confirmar Exclusão"
+          open={!!deleteRecord}
+          onCancel={() => setDeleteRecord(null)}
+          onOk={() => handleDeletePlan(deleteRecord?.key)}
+          okText="Excluir"
+          cancelText="Cancelar"
+          okButtonProps={{ danger: true }}
+        >
+          <p>Tem certeza de que deseja excluir o plano "{deleteRecord?.nome}"?</p>
         </Modal>
       </div>
     </>
